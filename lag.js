add9ea5d7a934275661c352310a3a2cd76718df6
jhnbg rth <I1234itsplaz@outlook.com>
2022-11-18 13:42:12 -0500

    Create lag.js

---
+++ lag.js
@@ -0,0 +1,29 @@
+class Lag { 
+   constructor(random1, random2) {
+    this.random1 = random1;
+    this.random2 = random2;
+  }
+}
+var battery1;
+var level;
+var levelStr;
+var levelperc;
+var lag
+function batteryPercent() {
+  navigator.getBattery().then(function(battery) {
+
+    level = battery.level;
+    levelStr = level.toString()
+    levelperc = levelStr.substring(levelStr.indexOf('.')+1)
+    document.getElementById("batteryPerc").innerHTML = levelperc+"%"
+});
+}
+function burnBattery() {
+ while (1==1) {
+   lag+=2;
+     document.getElementById("lag").innerHTML = Math.acosh(lag)
+ }
+}
+setInterval(function(update) {
+ batteryPercent();
+}, 1000)
